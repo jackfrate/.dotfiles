d3cac8bba0ab7a6466973eb4d2bb32670b9f879a
Jack Frate <jack.frate24@gmail.com>
2023-07-05 01:54:43 -0500

    get rid of copilot stuff

--- nvim/lua/config/vscode/autocommands.lua
+++ nvim/lua/config/vscode/autocommands.lua
@@ -3,8 +3,4 @@ augroup LineNumberManagementVscode
   autocmd InsertEnter * call VSCodeNotify('settings.cycle.lineNumbersOn')
   autocmd InsertLeave * call VSCodeNotify('settings.cycle.lineNumbersRelative')
 augroup END
-augroup CopilotToggleVscode
-  autocmd InsertEnter * call VSCodeNotify('settings.cycle.copilotOn')
-  autocmd InsertLeave * call VSCodeNotify('settings.cycle.copilotOff')
-augroup END
 ]])
